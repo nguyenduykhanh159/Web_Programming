9c9ab6ca51e3e7e9cf7ac53355a30866226105ba
quanpx <quanphungxuan00@gmail.com>
2021-12-03 10:38:54 +0700

    update cart controller

--- HappyFarmer/src/main/java/com/example/demo/dao/MockDataRepo.java
+++ HappyFarmer/src/main/java/com/example/demo/dao/MockDataRepo.java
@@ -3,6 +3,8 @@ package com.example.demo.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.demo.model.CartModel;
+import com.example.demo.model.ProductDetail;
 import com.example.demo.model.ProductModel;
 import com.example.demo.model.UserModel;
 
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Repository;
 public class MockDataRepo{
     private static List<UserModel> users=new ArrayList<UserModel>();
     private static List<ProductModel> products=new ArrayList<ProductModel>();
+    private static List<CartModel> carts=new ArrayList<CartModel>();
     public MockDataRepo()
     {
         UserModel user1=new UserModel();
@@ -43,6 +46,51 @@ public class MockDataRepo{
         products.add(prod2);
         products.add(prod3);
         products.add(prod4);
+
+        ProductDetail prodDetail1=new ProductDetail();
+        prodDetail1.setProductId(1);
+        prodDetail1.setPrice(30.7f);
+        prodDetail1.setQuantity(20);
+        ProductDetail prodDetail2=new ProductDetail();
+        prodDetail2.setProductId(3);
+        prodDetail2.setPrice(30.7f);
+        prodDetail2.setQuantity(20);
+        ProductDetail prodDetail3=new ProductDetail();
+        prodDetail3.setProductId(3);
+        prodDetail3.setPrice(30.7f);
+        prodDetail3.setQuantity(20);
+        ProductDetail prodDetail4=new ProductDetail();
+        prodDetail4.setProductId(4);
+        prodDetail4.setPrice(30.7f);
+        prodDetail4.setQuantity(20);
+
+        //Cart 1
+        CartModel cart1=new CartModel();
+        cart1.setCartId(1);
+        List<ProductDetail> prodsInCart1=new ArrayList<ProductDetail>();
+        prodsInCart1.add(prodDetail1);
+        prodsInCart1.add(prodDetail2);
+        cart1.setProducts(prodsInCart1);
+
+        //Cart 2
+        CartModel cart2=new CartModel();
+        cart2.setCartId(2);
+        List<ProductDetail> prodsInCart2=new ArrayList<ProductDetail>();
+        prodsInCart2.add(prodDetail1);
+        prodsInCart2.add(prodDetail3);
+        cart2.setProducts(prodsInCart2);
+
+        //Cart 3
+        CartModel cart3=new CartModel();
+        cart3.setCartId(3);
+        List<ProductDetail> prodsInCart3=new ArrayList<ProductDetail>();
+        prodsInCart3.add(prodDetail2);
+        prodsInCart3.add(prodDetail4);
+        cart3.setProducts(prodsInCart3);
+
+        carts.add(cart1);
+        carts.add(cart2);
+        carts.add(cart3);
 
 
 
@@ -87,6 +135,38 @@ public class MockDataRepo{
     public void addProduct(ProductModel prod)
     {
         products.add(prod);
+    }
+
+
+    //Cart info
+    public CartModel getCartInfo(int cartId)
+    {
+        for(CartModel cart : carts)
+        {
+            if(cart.getCartId()==cartId)
+            {
+                return cart;
+            }
+        }
+        return null;
+    }
+    public List<CartModel> getAllCarts()
+    {
+        return carts;
+    }
+    public void addCart(CartModel cart)
+    {
+        carts.add(cart);
+    }
+    public boolean addProductToCart(int cartId,ProductDetail productDetail)
+    {
+        CartModel cart=getCartInfo(cartId);
+        if(cart!=null)
+        {
+            cart.getProducts().add(productDetail);
+            return true;
+        }
+        return false;
     }
     
 }
---
+++ HappyFarmer/src/main/java/com/example/demo/service/impl/CartServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import com.example.demo.dao.MockDataRepo;
+import com.example.demo.model.CartModel;
+import com.example.demo.service.CartService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class CartServiceImpl implements CartService{
+    private MockDataRepo cartRepo;
+    @Autowired
+    public CartServiceImpl(MockDataRepo repo)
+    {
+        this.cartRepo=repo;
+    }
+    @Override
+    public CartModel getCartInfo(int cartId) {
+        // TODO Auto-generated method stub
+        
+        return cartRepo.getCartInfo(cartId);
+    }
+
+    @Override
+    public List<CartModel> getAllCarts() {
+        // TODO Auto-generated method stub
+        return cartRepo.getAllCarts();
+    }
+    @Override
+    public void addCart(CartModel cart) {
+        // TODO Auto-generated method stub
+        cartRepo.addCart(cart);
+        
+    }
+    
+}
