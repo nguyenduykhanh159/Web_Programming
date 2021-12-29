c49c3015a2bc1006b44f0a34613d979ce5d2f4f5
quanpx <quanphungxuan00@gmail.com>
2021-12-29 16:24:42 +0700

    add login url

--- src/main/java/com/example/demo/controller/home/HomeController.java
+++ src/main/java/com/example/demo/controller/home/HomeController.java
@@ -10,16 +10,12 @@ import com.example.demo.dto.user.UserDTO;
 import com.example.demo.service.user.CustomUserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 public class HomeController {
@@ -27,7 +23,7 @@ public class HomeController {
     @Autowired
     private CustomUserService customUserService;
 
-    @GetMapping(value = "/login")
+    @PostMapping(value = "/login")
     public BaseResponse login(@RequestBody UserDTO userDTO) {
         return customUserService.login(userDTO);
     }
