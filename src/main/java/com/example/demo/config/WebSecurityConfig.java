80f4acfc24921aaca1a55c80395822f1ba166bcb
quanpx <quanphungxuan00@gmail.com>
2021-12-30 16:35:19 +0700

    permit request to job product

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import com.example.demo.service.user.CustomUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -61,7 +62,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .csrf().disable()
         .authorizeRequests()
     //    .antMatchers("/**").permitAll(); 
-        .antMatchers("/login","/register").permitAll()
+        .antMatchers("/","/login","/register").permitAll()
+        .antMatchers(HttpMethod.GET, "/job").permitAll()
+        .antMatchers(HttpMethod.GET, "/job/detail/{id}").permitAll()
+        .antMatchers(HttpMethod.GET, "/product").permitAll()
+        .antMatchers(HttpMethod.GET, "/product/{id}").permitAll()
         .anyRequest().authenticated().and()
         .exceptionHandling().authenticationEntryPoint(jwtTokenEntryPoint).and();
 
